3153a1a42424da09aa73ea574b3e828cd86787de
Ticlext-Altihaf <yusufsekhanalthaf@gmail.com>
2024-07-01 17:18:06 +0200

    feat: Add migration for audit trails and simplify service provider
    - Create a migration for the `audit_trails` table to track model changes.
    - Replace `PackageServiceProvider` with `ServiceProvider` for a simplified service provider.
    - Update publishing configuration for migrations within `LaravelModelAuditServiceProvider`.

--- src/LaravelModelAuditServiceProvider.php
+++ src/LaravelModelAuditServiceProvider.php
@@ -3,70 +3,20 @@
 namespace BeraniDigitalID\LaravelModelAudit;
 
 use BeraniDigitalID\LaravelModelAudit\Commands\LaravelModelAuditCommand;
-use BeraniDigitalID\LaravelModelAudit\Listeners\AuditListener;
-use Illuminate\Filesystem\Filesystem;
-use Illuminate\Support\Facades\Event;
-use Spatie\LaravelPackageTools\Commands\InstallCommand;
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Illuminate\Support\ServiceProvider;
 
-class LaravelModelAuditServiceProvider extends PackageServiceProvider
+class LaravelModelAuditServiceProvider extends ServiceProvider
 {
     public static string $name = 'laravel-model-audit';
 
     public static string $viewNamespace = 'laravel-model-audit';
 
-    public function configurePackage(Package $package): void
+    public function boot(): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package->name(static::$name)
-            ->hasCommands($this->getCommands())
-            ->hasInstallCommand(function (InstallCommand $command) {
-                $command
-                    ->publishConfigFile()
-                    ->publishMigrations()
-                    ->askToRunMigrations()
-                    ->askToStarRepoOnGitHub('beranidigital/laravel-model-audit');
-            });
-
-        $configFileName = $package->shortName();
-
-        if (file_exists($package->basePath("/../config/{$configFileName}.php"))) {
-            $package->hasConfigFile();
-        }
-
-        $package->hasMigrations($this->getMigrations());
-
-        if (file_exists($package->basePath('/../resources/lang'))) {
-            $package->hasTranslations();
-        }
-
-        if (file_exists($package->basePath('/../resources/views'))) {
-            $package->hasViews(static::$viewNamespace);
-        }
+        $this->publishes([
+            __DIR__ . '/../database/migrations/' => database_path('migrations'),
+        ], 'laravel-model-audit-migrations');
     }
-
-    public function packageRegistered(): void {}
-
-    public function packageBooted(): void
-    {
-
-        // Handle Stubs
-        if (app()->runningInConsole()) {
-            foreach (app(Filesystem::class)->files(__DIR__ . '/../stubs/') as $file) {
-                $this->publishes([
-                    $file->getRealPath() => base_path("stubs/laravel-model-audit/{$file->getFilename()}"),
-                ], 'laravel-model-audit-stubs');
-            }
-        }
-
-    }
-
-
 
     protected function getAssetPackageName(): ?string
     {
